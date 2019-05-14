c691b0ddd009a78fe8294092e79a7ca9905db55b
Teddy Kornowitz <teddy.kornowitz@lyreco.com>
2019-05-14 17:47:10 +0200

    first commit

---
+++ src/main/java/com/lyreco/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,8 @@
+package com.lyreco.fizzbuzz;
+
+public class FizzBuzz {
+
+    public String fizzBuzz(Integer number){
+        return null;
+    }
+}
---
+++ src/main/java/com/lyreco/fizzbuzz/FizzbuzzApplication.java
@@ -0,0 +1,13 @@
+package com.lyreco.fizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzApplication.class, args);
+	}
+
+}
