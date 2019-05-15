536ce6fd2233969fa08a87aaa279ad1fe8dae4b2
Teddy Kornowitz <teddy.kornowitz@lyreco.com>
2019-05-15 08:38:05 +0200

    solution

--- src/main/java/com/lyreco/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/lyreco/fizzbuzz/FizzBuzz.java
@@ -1,8 +1,16 @@
 package com.lyreco.fizzbuzz;
 
+import java.util.Optional;
+
 public class FizzBuzz {
 
     public String fizzBuzz(Integer number){
-        return null;
+        return Optional.of(number).map(n -> {
+            if (n.toString().contains("3")) return "Lucky";
+            else if (n % 15 == 0) return "FizzBuzz";
+            else if (n % 3 == 0) return "Fizz";
+            else if (n % 5 == 0) return "Buzz";
+            else return n.toString();
+        }).get();
     }
 }
